0c69430d0ce0a9e77e2efd00020c981646717a22
zurielprogramador <argenis.canton08@gmail.com>
2023-11-08 18:48:24 -0500

    video17

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
   styleUrls: ['./reactive.component.css']
 })
-export class ReactiveComponent {
+export class ReactiveComponent implements OnInit {
+
+
+  forma: FormGroup;
+
+  constructor( private fb: FormBuilder){
+
+    this.crearFormulario();
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  crearFormulario() {
+
+    this.forma = this.fb.group({
+      nombre  : ['Zuriel'],
+      apellido: ['Canton'],
+      correo  : ['abc@abc.com'],
+
+    });
+
+  }
+
+
+
+  guardar(){
+    console.log( this.forma );
+  }
 
 }
