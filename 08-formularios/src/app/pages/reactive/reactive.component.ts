2227f43544db2e398035a8370897bfcb44cf97bb
zurielprogramador <argenis.canton08@gmail.com>
2023-11-08 18:56:16 -0500

    video19

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -20,6 +20,18 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get nombreNoValido() {
+    return this.forma.get('nombre')?.invalid && this.forma.get('nombre')?.touched
+  }
+
+  get apellidoNoValido() {
+    return this.forma.get('apellido')?.invalid && this.forma.get('apellido')?.touched
+  }
+
+  get correoNoValido() {
+    return this.forma.get('correo')?.invalid && this.forma.get('correo')?.touched
+  }
+
   crearFormulario() {
 
     this.forma = this.fb.group({
@@ -35,6 +47,12 @@ export class ReactiveComponent implements OnInit {
 
   guardar(){ 
     console.log( this.forma );
+
+    if( this.forma.invalid){
+      Object.values( this.forma.controls).forEach(control =>{
+        control.markAllAsTouched();
+      });
+    }
   }
  
 }
