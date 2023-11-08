d90fdd37dc135ce60282e68b8f7efff069eb528a
zurielprogramador <argenis.canton08@gmail.com>
2023-11-08 18:00:48 -0500

    video6

--- 08-formularios/src/app/pages/template/template.component.ts
+++ 08-formularios/src/app/pages/template/template.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.css']
 })
-export class TemplateComponent {
+export class TemplateComponent implements OnInit {
 
+  constructor( ) {}
+  ngOnInit():void{
+
+  }
+
+  guardar(){
+    console.log("submit disparado");
+  }
 }
