977755bc04a902026f8e1124bac38ff02775a543
zurielprogramador <argenis.canton08@gmail.com>
2023-11-08 21:20:59 -0500

    video26

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ValidadoresService } from 'src/app/services/validadores.service';
+import { ValidadoresService } from '../../services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -9,8 +9,9 @@ import { ValidadoresService } from 'src/app/services/validadores.service';
 })
 export class ReactiveComponent implements OnInit {
 
-  
+
   forma: FormGroup;
+
 
   constructor( private fb: FormBuilder,
                private validadores: ValidadoresService){
@@ -23,8 +24,8 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  get pasatiempos() { 
-    return this.forma.get('pasatiempos') as FormArray;
+  get pasatiempos (){
+    return this.forma.get('pasatiempos') as FormArray ;
   }
 
   get nombreNoValido() {
@@ -47,20 +48,39 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('direccion.ciudad')?.invalid && this.forma.get('direccion.ciudad')?.touched
   }
 
+  get pass1NoValido() {
+    return this.forma.get('pass1')?.invalid && this.forma.get('pass1')?.touched
+  }
+
+  get pass2NoValido() {
+    const pass1 = this.forma.get('pass1')?.value ;
+    const pass2 = this.forma.get('pass2')?.value;
+
+    return ( pass1 === pass2 ) ? false : true;
+
+  }
+
+
+
+
   crearFormulario() {
 
     this.forma = this.fb.group({
       nombre  : ['', [ Validators.minLength(5), Validators.required  ] ],
-      apellido: ['', [ Validators.required, this.validadores.noHerrera ] ],
+      apellido: ['', Validators.required ],
       correo  : ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')] ],
+      pass1   : ['',  Validators.required],
+      pass2   : ['',  Validators.required],
       direccion: this.fb.group({
         distrito: ['', Validators.required],
         ciudad  : ['', Validators.required],
       }),
-      pasatiempos: this.fb.array([])
-
+      pasatiempos: this.fb.array([
+      ])
+    },{
+      Validators: this.validadores.passwordsIguales( 'pass1', 'pass2')
     });
-  
+
   }
 
 
@@ -79,14 +99,17 @@ export class ReactiveComponent implements OnInit {
   }
 
   agregarPasatiempo(){
-    this.pasatiempos.push( this.fb.control('') );
+    this.pasatiempos.push( this.fb.control('Nuevo elemento', Validators.required));
   }
 
   borrarPasatiempos(i : number){
     this.pasatiempos.removeAt(i);
   }
 
-  guardar(){ 
+
+
+
+  guardar(){
     console.log( this.forma );
 
     if( this.forma.invalid){
@@ -97,10 +120,10 @@ export class ReactiveComponent implements OnInit {
     }
 
 
-    // Posteo de informacion 
+    // Posteo de informacion
     this.forma.reset({
       nombre: 'sin nombre'
     });
   }
- 
+
 }
--- 08-formularios/src/app/services/validadores.service.ts
+++ 08-formularios/src/app/services/validadores.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidadoresService {
 
-
   constructor() { }
 
-
-  noHerrera(control: FormControl ): {[s: string ]: any } {
+  noHerrera(control: FormControl ): {[s: string ]: boolean | any } {
 
 
     if (control.value?.toLowerCase() === 'herrera') {
@@ -20,5 +18,19 @@ export class ValidadoresService {
     }
 
     return {noHerrera: null};
+  }
+
+  passwordsIguales( pass1Name: string, pass2Name: string ){
+    return ( formGroup: FormGroup ) => {
+
+      const pass1Control = formGroup.controls[pass1Name];
+      const pass2Control = formGroup.controls[pass1Name];
+
+      if ( pass1Control.value === pass2Control.value ) {
+        pass2Control.setErrors(null);
+      } else {
+        pass2Control.setErrors({ noEsIgual: true});
+      }
+    }
   }
 }
